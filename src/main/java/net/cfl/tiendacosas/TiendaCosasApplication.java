bfd998963f94202d259a12debd999a1f22a12bf6
Programacion8 <darioramos8919@gmail.com>
2024-10-04 10:31:05 -0300

    Commit inicial

---
+++ src/main/java/net/cfl/tiendacosas/TiendaCosasApplication.java
@@ -0,0 +1,13 @@
+package net.cfl.tiendacosas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaCosasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaCosasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/cfl/tiendacosas/TiendaCosasApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cfl.tiendacosas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaCosasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
