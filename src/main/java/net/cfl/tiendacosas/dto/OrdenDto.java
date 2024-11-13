b628e45392711a50d041bac5b4d9c4b6b802fa8d
Programacion8 <darioramos8919@gmail.com>
2024-11-13 11:44:23 -0300

    Agregado las caracteristicas faltantes en ordenDto

--- src/main/java/net/cfl/tiendacosas/dto/OrdenDto.java
+++ src/main/java/net/cfl/tiendacosas/dto/OrdenDto.java
@@ -1,5 +1,19 @@
 package net.cfl.tiendacosas.dto;
 
-public class OrdenDto {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
+import lombok.Data;
+import net.cfl.tiendacosas.enums.OrdenEstado;
+
+@Data
+public class OrdenDto {
+	private Long id;
+	private Long usuarioId;
+	private LocalDate ordenFecha;
+	private BigDecimal montoTotal;
+	private OrdenEstado ordenEstado;
+	private List<OrdenItemDto> items;
+	
 }
