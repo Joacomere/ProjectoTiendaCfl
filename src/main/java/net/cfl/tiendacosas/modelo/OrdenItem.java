ccc0320553482e322cc0b6a49800a751c23eb64a
Programacion8 <darioramos8919@gmail.com>
2024-11-20 09:37:28 -0300

    Implementada la anotacion Id en la entidad ordenItem

--- src/main/java/net/cfl/tiendacosas/modelo/OrdenItem.java
+++ src/main/java/net/cfl/tiendacosas/modelo/OrdenItem.java
@@ -4,6 +4,9 @@ package net.cfl.tiendacosas.modelo;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
@@ -15,6 +18,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 public class OrdenItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@ManyToOne
